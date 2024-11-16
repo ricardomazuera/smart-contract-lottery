7ee1694120a57b1b061da00dfe712ef1adb3c8ad
Ricardo Mazuera <ricardo.mazuera@gmail.com>
2024-11-16 12:36:25 -0500

    added some tests

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -18,6 +18,11 @@ contract RaffleTest is Test {
     uint32 callbackGasLimit;
 
     address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
 
     function setUp() external {
         DeployRaffle deployRaffle = new DeployRaffle();
@@ -29,9 +34,58 @@ contract RaffleTest is Test {
         keyHash = config.keyHash;
         subscriptionId = config.subscriptionId;
         callbackGasLimit = config.callbackGasLimit;
+
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
     }
 
     function testRaffleInitializesInOpenState() public view {
         assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
     }
+
+    function testRaffleRevertsWhenPlayerDontPayEnough() public {
+        // Arrange
+        vm.prank(PLAYER);
+
+        // Act / Assert
+        vm.expectRevert(Raffle.Raffle__SendMoreToPayEnterRaffle.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayersWhenTheyEnter() public {
+        // Arrange
+        vm.prank(PLAYER);
+
+        // Act
+        raffle.enterRaffle{value: entranceFee}();
+
+        // Assert
+        address playerRecorded = raffle.getPlayers(0);
+        assert(playerRecorded == PLAYER);
+    }
+
+    function testEnteringRaffleEmitsEvent() public {
+        // Arrange
+        vm.prank(PLAYER);
+
+        // Act
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit RaffleEntered(PLAYER);
+        // Assert
+        raffle.enterRaffle{value: entranceFee}();
+    }
+
+    // function testDontAllowPlayersToEnterWhileRaffleIsCalculating() public {
+    //     // Arrange
+    //     vm.prank(PLAYER);
+    //     raffle.enterRaffle{value: entranceFee}();
+    //     vm.warp(block.timestamp + interval + 1); // This will make the automation be activated and the raffle will be calculating
+    //     vm.roll(block.number + 1); // This simulates the block where the automation is activated
+
+    //     raffle.performUpkeep("");
+
+    //     // Act / Assert
+    //     vm.expectRevert(Raffle.Raffle__RaffleIsNotOpen.selector);
+    //     vm.prank(PLAYER);
+    //     raffle.enterRaffle{value: entranceFee}();
+    // }
 }
